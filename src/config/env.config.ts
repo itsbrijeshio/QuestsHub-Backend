5d7d0d75b2631f6b7c588c0ebabebd2040d2678e
itsbrijeshio <bk6500416@gmail.com>
2025-07-15 21:18:52 +0530

    feat(config): add jwt secrets

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -8,6 +8,8 @@ const env = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
 };
 
 export default env;
